420362a3bd1c5a5b901fe8bfa28afad4f7d59bfa
XXXRef <xxxref@xxxref.com>
2026-01-06 01:43:45 +0300

    feature: introduce version info handling

--- code/app/src/Components/Footer.jsx
+++ code/app/src/Components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import verCfg from '../../app_config.json' with { type: 'json' };
+
+
+const versionInfo = verCfg.versionInfo;
+
 
 export default function Footer() {
   return (
     <div id="div_footer">
-      <div id="div_footer_leftpart">atlt v1.1</div>
+      <div id="div_footer_leftpart">
+        atlt v{versionInfo.versionNumber} b:{versionInfo.branch} base_commit_id:{versionInfo.commit}
+      </div>
       <div id="div_footer_rightpart"><span><a href="https://oiledowl.com">OiledOwl corp.</a> , 2022</span></div>
     </div>
   )
